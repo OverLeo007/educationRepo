dec9a88aa8b23915e6c67e22eae11a8b4bf2c396
OverLeo007 <sokolov_lev2003@mail.ru>
2021-09-06 20:56:44 +0700

    upd

--- pract1_var26.py
+++ pract1_var26.py
@@ -9,13 +9,27 @@ def eq(a, b):
         return False
 
 
+def rounder(n: int):
+    if isinstance(n, float):
+        num = str(n).split('.')
+        if num[1] == '0':
+            return int(n)
+        elif num[1].count('9') > 12:
+            return int(num[0]) + 1
+        elif num[1].count('0') > 12:
+            return int(num[0])
+    return n
+
+
 # a, b, ang1 = map(int, input().split())
-a, b, ab = 3, 5, 90
+a, b, ab = 3, 4, 90
 ab = radians(ab)
 c = (a ** 2 + b ** 2 - 2 * a * b * cos(ab)) ** 0.5
 bc = asin((a * sin(ab) / c))
 ac = asin((b * sin(bc)) / a)
-ab, bc, ac = degrees(ab), degrees(bc), degrees(ac)
+# ab, bc, ac = degrees(ab), degrees(bc), degrees(ac)
+ab, bc, ac = map(lambda x: rounder(x), (degrees(ab), degrees(bc), degrees(ac)))
+a, b, c = map(lambda x: rounder(x), (a, b, c))
 print(a, b, c)
 print(ab, bc, ac)
 sides = (a, b, c)
