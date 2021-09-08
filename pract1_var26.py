3c92545d4c6eeb84fad6c6e9076d6689118eede4
OverLeo007 <sokolov_lev2003@mail.ru>
2021-09-08 22:44:02 +0700

    final ver

--- pract1_var26.py
+++ pract1_var26.py
@@ -1,10 +1,11 @@
 import sys
+
 from math import cos, degrees, radians, sin, asin
 from itertools import combinations
 
 
-
 def eq(ea, eb):
+    """Функция, производящее нечеткое сравнение данных ей аргументов"""
     if abs(ea - eb) < 0.000000000001:
         return True
     else:
@@ -12,6 +13,8 @@ def eq(ea, eb):
 
 
 def my_is_num(num: str):
+    """Проверка строки на то, является ли ее содержимое числом
+    (в отличии от стандартных методов строки поддерживает отрицательные значения)"""
     try:
         float(num)
         return True
@@ -20,6 +23,7 @@ def my_is_num(num: str):
 
 
 def main(a, b, ab):
+    """Тело программы, на вход поступают корректные значения и обрабатываются"""
     ab = radians(ab)
     c = (a ** 2 + b ** 2 - 2 * a * b * cos(ab)) ** 0.5
     bc = asin((a * sin(ab) / c))
@@ -29,19 +33,17 @@ def main(a, b, ab):
     angles = (ab, bc, ac)
     sides_comp = list(combinations((a, b, c), 2))
 
+    is_find = False
     sn = 0
     for i in sides_comp:
         if eq(*i):
             sn += 1
-    is_find = False
     if sn == 1:
         is_find = True
         print('\x1b[32m[RES]\x1b[0mТреугольник равнобедренный')
-
     elif sn == 3:
         is_find = True
         print('\x1b[32m[RES]\x1b[0mТреугольник равносторонний')
-
     for i in angles:
         if eq(90, i):
             is_find = True
@@ -53,6 +55,7 @@ def main(a, b, ab):
 is_test_mode = False
 while (test := input('Запустить режим ввода примеров? Y/N\n\x1b[33m[INP]\x1b[0m')) not in 'YN':
     continue
+
 if test == 'Y':
     print('\x1b[32mЗапускаю...\x1b[0m')
     sys.stdin = open('test_input.txt', 'r')
@@ -62,15 +65,14 @@ elif test == 'N':
 
 while 'exit' not in (x := input('Введите входные данные или "exit" для завершения работы программы\n'
                                 '\x1b[33m[INP]\x1b[0m').split(' ')):
-
-    is_nums = True
-    is_val_corr = True
-
     if is_test_mode:
         print(f'\x1b[32m{" ".join(x)}\x1b[0m')
+
     if len(x) > 3:
         print('\x1b[31m[ERR]\x1b[0mВведено больше значений чем требуется')
         continue
+
+    is_nums = True
     for i in x:
         if not my_is_num(i):
             print('\x1b[31m[ERR]\x1b[0mВведен не числовой тип данных')
@@ -79,6 +81,7 @@ while 'exit' not in (x := input('Введите входные данные ил
     if is_nums is False:
         continue
 
+    is_val_corr = True
     side1, side2, angle = map(lambda z: float(z) if '.' in z else int(z), x)
     if side1 <= 0 or side2 <= 0:
         print('\x1b[31m[ERR]\x1b[0mСтороны треугольника не могут быть отрицательными')
@@ -88,7 +91,9 @@ while 'exit' not in (x := input('Введите входные данные ил
         is_val_corr = False
     if not is_val_corr:
         continue
+
     main(side1, side2, angle)
 if is_test_mode:
     print('exit')
+
 print('\x1b[36m[FIN]\x1b[0mПрограмма успешно завершена')
