9b1422eb27f95a2a6f41b3ec013653df180d38cd
OverLeo007 <sokolov_lev2003@mail.ru>
2021-09-13 21:33:21 +0700

    new variant

--- subset_sum.py
+++ subset_sum.py
@@ -1,38 +1,52 @@
-# A recursive solution for subset sum
-# problem
+# Python program for the above approach
 
-# Returns true if there is a subset
-# of set[] with sun equal to given sum
-
-
-def is_subset_sum(mn, n, sm):
-    # Base Cases
-    if sm == 0:
-        return True
-    if n == 0:
-        return False
-
-    # If last element is greater than
-    # sum, then ignore it
-    if mn[n - 1] > sm:
-        return isSubsetSum(mn, n - 1, sm)
-
-    # else, check if sum can be obtained
-    # by any of the following
-    # (a) including the last element
-    # (b) excluding the last element
-    return is_subset_sum(
-        mn, n - 1, sm) or is_subset_sum(
-        mn, n - 1, sm - mn[n - 1])
+# Taking the matrix as globally
+tab = [[-1 for i in range(2000)] for j in range(2000)]
 
 
-# Driver code
-set = [3, 34, 4, 12, 5, 2]
-sum = 9
-n = len(set)
-if isSubsetSum(set, n, sum) is True:
-    print("Found a subset with given sum")
+# Check if possible subset with
+# given sum is possible or not
+def subsetSum(a, n, summ):
+    # If the sum is zero it means
+    # we got our expected sum
+    if summ == 0:
+        return 1
+
+    if n <= 0:
+        return 0
+
+    # If the value is not -1 it means it
+    # already call the function
+    # with the same value.
+    # it will save our from the repetition.
+    if tab[n - 1][summ] != -1:
+        return tab[n - 1][summ]
+
+    # if the value of a[n-1] is
+    # greater than the sum.
+    # we call for the next value
+    if a[n - 1] > summ:
+        tab[n - 1][summ] = subsetSum(a, n - 1, summ)
+        return tab[n - 1][summ]
+    else:
+
+        # Here we do two calls because we
+        # don't know which value is
+        # full-fill our criteria
+        # that's why we doing two calls
+        tab[n - 1][summ] = subsetSum(a, n - 1, summ)
+        return tab[n - 1][summ] or subsetSum(a, n - 1, summ - a[n - 1])
+
+
+# Driver Code
+
+n = 5
+a = [1, 5, 3, 7, 4]
+sm = 2
+
+if subsetSum(a, n, sm):
+    print("YES")
 else:
-    print("No subset with given sum")
+    print("NO")
 
-# This code is contributed by Nikita Tiwari.
+# This code is contributed by shivani.
