3461ea9be7039e88e883439178b6ac6301e15449
OverLeo007 <Sokolov_lev2003@mail.ru>
2021-10-27 22:03:22 +0700

    ar_laba

---
+++ caesar_argprs.py
@@ -0,0 +1,12 @@
+import argparse
+from caesar import caesar
+
+
+parser = argparse.ArgumentParser(description='Сдвиг по алфавиту')
+parser.add_argument('text', type=str,
+                    help='Строка для зашифровки')
+parser.add_argument('-sh', '--shift', dest='shift', default=0, type=int,
+                    help='Сдвиг по алфавиту')
+args = parser.parse_args()
+
+print(f'Индекс элемента: {caesar(args.text, args.shift)}')
