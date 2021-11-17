cdd74b34d611217639848221638d12283db04ff7
Paskal <Vaxxo9000@gmail.com>
2021-11-17 22:11:17 +0700

    p4v26 fin

---
+++ p4v26.py
@@ -0,0 +1,81 @@
+class Graph:
+    def __init__(self):
+        self.start_n, self.end_n = None, None
+        self.node_adj = None
+
+    def set_way(self):
+        self.start_n, self.end_n = map(int, input('Введите начальный и конеченый узлы:\n').split(' '))
+        return 'Узлы успешно заданы'
+
+    def set_graph(self):
+        with open(input('Введите имя файла с матрицей:\n')) as mat:
+            wmatrix = list(map(lambda x: list(map(int, x.replace('\n', '').split(' '))), mat.readlines()))
+        self.node_adj = {}
+        for nnum, node in enumerate(wmatrix):
+            conns = []
+            for cnum, node_conn in enumerate(node):
+                if node_conn != 0:
+                    conns.append((cnum, node_conn))
+            self.node_adj[nnum] = tuple(conns)
+        return 'Матрца успешно задана'
+
+    def get_conn(self, node):
+        return self.node_adj[node]
+
+    def findway(self, tree):
+        res = [self.end_n]
+        cnode = self.end_n
+        while cnode != self.start_n:
+            cnode = tree[cnode]
+            res.append(cnode)
+        res.reverse()
+        return tuple(res)
+
+    def dijkstra(self):
+        print(self.start_n)
+        if self.node_adj is None:
+            return 'Матрица еще на задана'
+        elif self.start_n is None:
+            return 'Точки еще не заданы'
+        seen = set()
+        inf = float('inf')
+        weights = {i: [inf, 0] for i in self.node_adj.keys()}
+        if self.start_n not in weights.keys() or self.end_n not in weights.keys():
+            return 'Ошибка, начальный или конечный узел не присоединен к графу'
+        weights[self.start_n][0] = 0
+        while len(seen) != len(weights):
+            cur_node = min(filter(lambda x: x[0] not in seen, weights.items()), key=lambda x: x[1][0])[0]
+            cur_weight = weights[cur_node][0]
+            cnodes = self.get_conn(cur_node)
+            for node, weight in cnodes:
+                if node not in seen:
+                    tot_weight = cur_weight + weight
+                    if weights[node][0] > tot_weight:
+                        weights[node][0] = tot_weight
+                        weights[node][1] = cur_node
+            seen.add(cur_node)
+        tree = {node: data[1] for node, data in weights.items()}
+        weights = {node: data[0] for node, data in weights.items()}
+        return f'{weights[self.end_n]} {self.findway(tree)}'
+
+
+# while True:
+graph = Graph()
+menu = {'1': ('Задать имя файла с матрицей', graph.set_graph),
+        '2': ('Задать начальные и конечные узлы', graph.set_way),
+        '3': ('Просчитать кратчайший маршрут между узлами', graph.dijkstra),
+        '4': ('Завершить работу программы', exit)}
+
+while True:
+    printable_menu = '\n'.join(map(lambda y: ' - '.join(y), map(lambda x: (x[0], x[1][0]), menu.items())))
+    opt = input(f"Выберете опцию:\n{printable_menu}\n")
+    try:
+        print(menu.get(opt)[-1]())
+    except TypeError as e:
+        print(e)
+        print('Нет такого варианта выбора')
+    except ValueError:
+        print('Ошибка: матрица задана некорректно\nИЛИ\nНачальный и\\или конечный узел введен(ы) не корректно')
+    except FileNotFoundError:
+        print('Ошибка: Файла не существует')
+
